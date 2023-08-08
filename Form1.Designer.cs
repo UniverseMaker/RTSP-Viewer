a7bf980d3a47283841828438102459bca56a6d3e
Daeseung Park <eosonox@gmail.com>
2023-08-08 10:53:59 +0900

    2023 Version

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RTSPViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picVideo = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picVideo
+            // 
+            this.picVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picVideo.Location = new System.Drawing.Point(0, 0);
+            this.picVideo.Name = "picVideo";
+            this.picVideo.Size = new System.Drawing.Size(1169, 772);
+            this.picVideo.TabIndex = 0;
+            this.picVideo.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnStart.Location = new System.Drawing.Point(0, 772);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(1169, 45);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "View";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 817);
+            this.Controls.Add(this.picVideo);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "RTSP Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.picVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picVideo;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace RTSPViewer
+{
+    public partial class Form1 : Form
+    {
+        string rtspUrl = "rtsp://210.99.70.120:1935/live/cctv001.stream";
+        VideoCapture video = new VideoCapture();
+
+        public Form1()
+        {
+            InitializeComponent();
+            picVideo.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            video.Open(rtspUrl);
+
+            using (Mat image = new Mat())
+            {
+                while (true)
+                {
+                    if (!video.Read(image))
+                    {
+                        Cv2.WaitKey();
+                    }
+                    if (!image.Empty())
+                    {
+                        Bitmap bmp = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+                        picVideo.Image = bmp;
+                    }
+                    if (Cv2.WaitKey(1) >= 0)
+                        break;
+                }
+            }
+            video = null;
+        }
+    }
+}
